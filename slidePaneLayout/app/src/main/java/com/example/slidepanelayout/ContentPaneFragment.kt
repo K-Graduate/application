a3b105b7815e88a9ca85d1acef32d597a9d45245
minki_hong <gaogai2@naver.com>
2021-07-30 18:20:41 +0900

    add simple layout

---
+++ slidePaneLayout/app/src/main/java/com/example/slidepanelayout/ContentPaneFragment.kt
@@ -0,0 +1,18 @@
+package com.example.slidepanelayout
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+
+class ContentPaneFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.content_pane, container, false)
+        val close = view.findViewById<Button>(R.id.side_close)
+        close.setOnClickListener { (activity as MainActivity).slidingPane.close() }
+        //버튼 클릭시 slidingPane close기능
+        return view
+    }
+}
