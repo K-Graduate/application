ef3cd7fef33120689561f5af48922d91b6302c9c
HanYong <toy9910@naver.com>
2021-09-30 23:25:50 +0900

    BottomNavigationView 추가 및 바인딩 처리

--- KGraduate/app/src/main/java/com/example/kgraduate/layouts/MainActivity.kt
+++ KGraduate/app/src/main/java/com/example/kgraduate/layouts/MainActivity.kt
@@ -6,16 +6,23 @@ import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
+import androidx.navigation.findNavController
 import com.example.kgraduate.layouts.LoginActivity.Companion.TAG
 import com.example.kgraduate.R
+import com.example.kgraduate.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    
+    lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val prefs : SharedPreferences = getSharedPreferences("Prefs", Context.MODE_PRIVATE)
         Log.d(TAG, "onCreate: ${prefs.getString("token","")}")
     }
+
+
 }
