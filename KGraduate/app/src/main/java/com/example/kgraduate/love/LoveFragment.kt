b2bcee0195c174e3c7669fa78a584374014d7871
HanYong <toy9910@naver.com>
2021-10-01 20:36:42 +0900

    recycler view item 디자인 적용

--- KGraduate/app/src/main/java/com/example/kgraduate/love/LoveFragment.kt
+++ KGraduate/app/src/main/java/com/example/kgraduate/love/LoveFragment.kt
@@ -5,10 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.kgraduate.R
 import com.example.kgraduate.databinding.FragmentLoveBinding
+import com.example.kgraduate.posts.Post
+import com.example.kgraduate.posts.PostAdapter
+import kotlinx.android.synthetic.main.fragment_love.*
 
 class LoveFragment : Fragment() {
     lateinit var binding : FragmentLoveBinding
+    lateinit var postAdapter: PostAdapter
+    val datas = mutableListOf<Post>()
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -16,5 +22,18 @@ class LoveFragment : Fragment() {
     ): View? {
         binding = FragmentLoveBinding.inflate(inflater,container,false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        postAdapter = PostAdapter(requireContext())
+        rv_love.adapter = postAdapter
+
+        datas.apply {
+            add(Post(post_image = "https://i.ibb.co/3YD5hkV/YJH-2150-222.jpg", post_content = "건들면 뭅니다", post_like = "300", post_comment = "12"))
+            add(Post(post_image = "https://i.ibb.co/3YD5hkV/YJH-2150-222.jpg", post_content = "건들면 뭅니다", post_like = "300", post_comment = "12"))
+            add(Post(post_image = "https://i.ibb.co/3YD5hkV/YJH-2150-222.jpg", post_content = "건들면 뭅니다", post_like = "300", post_comment = "12"))
+            postAdapter.postdatas = datas
+            postAdapter.notifyDataSetChanged()
+        }
     }
 }
---
+++ KGraduate/app/src/main/java/com/example/kgraduate/posts/PostAdapter.kt
@@ -0,0 +1,51 @@
+package com.example.kgraduate.posts
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.kgraduate.R
+import kotlinx.android.synthetic.main.recycler_post_item.view.*
+
+class PostAdapter(private val context : Context) : RecyclerView.Adapter<PostAdapter.ViewHolder>() {
+    var postdatas = mutableListOf<Post>()
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostAdapter.ViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.recycler_post_item,parent,false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(postdatas[position])
+    }
+
+    override fun getItemCount(): Int = postdatas.size
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private val postImg : ImageView = itemView.iv_recylcer
+        private val postContent : TextView = itemView.tv_recycler
+        private val postLike : TextView = itemView.tv_like_count
+        private val postComment : TextView = itemView.tv_comment_count
+
+        fun bind(item: Post) {
+            Glide.with(itemView).load(item.post_image).into(postImg)
+            postContent.text = item.post_content
+            postLike.text = item.post_like
+            postComment.text = item.post_comment
+        }
+    }
+
+//        private val txtName: TextView = itemView.findViewById(R.id.tv_rv_name)
+//        private val txtAge: TextView = itemView.findViewById(R.id.tv_rv_age)
+//        private val imgProfile: ImageView = itemView.findViewById(R.id.img_rv_photo)
+//
+//        fun bind(item: ProfileData) {
+//            txtName.text = item.name
+//            txtAge.text = item.age.toString()
+//            Glide.with(itemView).load(item.img).into(imgProfile)
+//
+//        }
+}
